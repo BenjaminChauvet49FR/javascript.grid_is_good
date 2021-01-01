1af9d0e74dc48271041ea7eeaed3c912fe9bbe99
ChocophileBenj <benjchauvet@gmail.com>
2021-01-01 01:20:57 +0100

    Added Solving method packs

---
+++ SolvingMethodPacks.js
@@ -0,0 +1,12 @@
+function ApplyEventMethodPack(p_applyEventMethod, p_deductionsMethod, p_adjacencyMethod, p_transformMethod, p_undoEventMethod) {
+	this.applyEventMethod = p_applyEventMethod;
+	this.deductionsMethod = p_deductionsMethod;
+	this.adjacencyMethod = p_adjacencyMethod;
+	this.retrieveGeographicalDeductionMethod = p_transformMethod;
+	this.undoEventMethod = p_undoEventMethod;
+}
+
+ApplyEventMethodPack.prototype.addAbortAndFilters = function(p_abortMethod, p_filters) {
+	this.abortMethod = p_abortMethod
+	this.filters = p_filters;
+}
